ec5905cba4976796f1985cad694b97460968be2c
AlexandrebQueiroz <megalexandre@gmail.com>
2022-07-07 18:44:46 -0300

    dist

--- src/app/pages/operation/alert/alert-item/alert-item.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-alert-item',
-  styleUrls: ['./alert-item.component.scss'],
-  templateUrl: `./alert-item.component.html`,
-})
-export class AlertItemComponent {
-}
--- src/app/pages/operation/occurrence/_index.ts
+++
@@ -1,4 +0,0 @@
-export * from './occurrence-routing.module'
-export * from './occurrence.component'
-export * from './occurrence.module'
-export * from './occurrence.service'
